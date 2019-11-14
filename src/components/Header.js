05a535ad9de97fb03e8af736e9f73dad20662544
Kathryn Howle <kathrynhowle@Kathryns-MacBook-Pro.local>
2019-11-14 13:40:20 -0600

    Display full header on larger devices

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,28 +1,39 @@
 import React from 'react';
-import _ from 'lodash';
+import get from 'lodash/get';
+import map from 'lodash/map';
 
 import {classNames, Link, safePrefix} from '../utils';
 
 export default class Header extends React.Component {
     render() {
         return (
-            <header id="header" className={classNames({'alt': _.get(this.props, 'pageContext.frontmatter.template') === 'home'})}>
-                <h1><Link to={safePrefix('/')}>{_.get(this.props, 'pageContext.site.siteMetadata.title')}</Link></h1>
-                <nav id="nav">
+            <header id="header" className={classNames({'alt': get(this.props, 'pageContext.frontmatter.template') === 'home'})}>
+                <h1><Link to={safePrefix('/')}>{get(this.props, 'pageContext.site.siteMetadata.title')}</Link></h1>
+                <nav id="nav" className="mobile-menu">
                     <ul>
                         <li className="special">
                             <Link to="#menu" className="menuToggle"><span>Menu</span></Link>
                             <div id="menu">
                                 <ul>
-                                    {_.map(_.get(this.props, 'pageContext.menus.main'), (item, item_idx) => (
-                                        <li key={item_idx}><Link to={safePrefix(_.get(item, 'url'))}>{_.get(item, 'title')}</Link></li>
+                                    {map(get(this.props, 'pageContext.menus.main'), (item, item_idx) => (
+                                        <li key={item_idx}><Link to={safePrefix(get(item, 'url'))}>{get(item, 'title')}</Link></li>
                                     ))}
-                                    {_.map(_.get(this.props, 'pageContext.site.data.menu.actions'), (action, action_idx) => (
-                                        <li key={action_idx}><Link to={(_.get(action, 'url').startsWith('#') ? _.get(action, 'url') : safePrefix(_.get(action, 'url')))}>{_.get(action, 'label')}</Link></li>
+                                    {map(get(this.props, 'pageContext.site.data.menu.actions'), (action, action_idx) => (
+                                        <li key={action_idx}><Link to={(get(action, 'url').startsWith('#') ? get(action, 'url') : safePrefix(get(action, 'url')))}>{get(action, 'label')}</Link></li>
                                     ))}
                                 </ul>
                             </div>
                         </li>
+                    </ul>
+                </nav>
+                <nav id="nav" className="desktop-menu">
+                    <ul>
+                        {map(get(this.props, 'pageContext.menus.main'), (item, item_idx) => (
+                            <li key={item_idx}><Link to={safePrefix(get(item, 'url'))}>{get(item, 'title')}</Link></li>
+                        ))}
+                        {map(get(this.props, 'pageContext.site.data.menu.actions'), (action, action_idx) => (
+                            <li key={action_idx}><Link to={(get(action, 'url').startsWith('#') ? get(action, 'url') : safePrefix(get(action, 'url')))}>{get(action, 'label')}</Link></li>
+                        ))}
                     </ul>
                 </nav>
             </header>
